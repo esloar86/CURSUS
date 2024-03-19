89d7b3d84d5eabf1c02ec5ddb78023e96e8ee41d
esloar86 <esloar86@gmail.com>
2024-03-19 18:07:16 +0100

    nueva subida

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -1,34 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eslopez- <eslopez-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/18 17:36:28 by eslopez-          #+#    #+#             */
-/*   Updated: 2024/03/19 17:38:38 by fany             ###   ########.fr       */
+/*   Created: 2024/03/19 17:37:02 by eslopez-          #+#    #+#             */
+/*   Updated: 2024/03/19 18:05:29 by fany             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include<ctype.h>
-//#include<string.h> 
-//#include<stdio.h>
+//#include <stdio.h>
+//#include <string.h>
 #include <libft.h>
 
-int	ft_strlen(char *str)
+void	*ft_memset(void *ptr, int value, size_t num_len)
 {
-	int i;
+	size_t i;
 
 	i = 0;
-	while (*(str+i) != '\0')
+	while (i < num_len)
+	{
+		((unsigned char *)ptr)[i] = value;
 		i++;
-	return (i);
+	}
+	return (ptr);
 }
 /*
-int	main(void)
-{
-	char str[] = "patata";
-	int result = ft_strlen(str);
-	printf("La longitud con strlen es %zu \n", strlen(str));
-	printf("La longitud con ft_strlen es %zu \n", result);
+int main() {
+    char prueba[10];
+    char prueba2[10];
+    memset(prueba, 0, sizeof(prueba));
+    ft_memset(prueba2, 0, sizeof(prueba2));
+    for (int i = 0; i < 10; ++i) {
+        printf("%d ", prueba[i]);
+	printf("%d \n", prueba2[i]);
+    }
+    return 0;
 }*/
